92fba3d6079d9c1ec677781a5a0d1553b93b74c5
shenrique08 <sergimmatos@hotmail.com>
2024-09-17 14:38:49 -0300

    Passos iniciais

--- 1-JavaProcedimental/src/Main.java
+++ 1-JavaProcedimental/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        int teste = 10;
+        double teste2 = 1.2354353;
+        System.out.println(teste);
+        System.out.println("oi " + teste);
+        System.out.printf("%d\n", teste);
+        System.out.printf("%.3f\n", teste2);
         System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.3f\n", teste2);
     }
 }
---
+++ 1-JavaProcedimental/src/ex01/Ex01.java
@@ -0,0 +1,29 @@
+package ex01;
+
+import java.util.Locale;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        String product01 = "Computer";
+        String product02 = "Mouse";
+
+        int age = 30;
+        int code = 2354;
+        char gender = 'F';
+
+        double price1 = 2999.99;
+        double price2 = 650.233;
+        double measure = 53.352334;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which the price is $ %.2f\n", product01, price1);
+        System.out.printf("%s, which the price is $ %.2f\n", product02, price2);
+
+        System.out.printf("Record: %d years old, code %d and gender %c\n", age, code, gender);
+        System.out.printf("Measure with 8 decimal places: %.8f\n", measure);
+        System.out.printf("Measure with 3 decimal places: %.3f\n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f\n", measure);
+    }
+
+}
