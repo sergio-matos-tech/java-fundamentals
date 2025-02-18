82f5b502dd9d5aa7652a7fb4da6142658a8bb4fe
shenrique08 <sergimmatos@hotmail.com>
2025-02-18 17:39:42 -0300

    finishing up exception practice

--- 8-Exceptions/ex01/src/application/Main.java
+++ 8-Exceptions/ex01/src/application/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         List<Reservation> reservationsList = new ArrayList<>();
 
-        System.out.println("*** RESERVATION ***");
+        System.out.println("*** BOOKING ***");
         System.out.print("Room Number: ");
         Integer roomNumber = input.nextInt();
         input.nextLine();
@@ -26,6 +26,7 @@ public class Main {
 
         LocalDate checkinDate = LocalDate.parse(checkinDateString, dateTimeFormatter);
         LocalDate checkoutDate = LocalDate.parse(checkoutDateString, dateTimeFormatter);
+
         Reservation reservation01 = new Reservation(roomNumber, checkinDate, checkoutDate);
         reservationsList.add(reservation01);
         for (Reservation r : reservationsList) {
@@ -33,7 +34,17 @@ public class Main {
         }
 
         // update the reservation
-
+        System.out.println("\nUPDATE DATES");
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        checkinDateString = input.nextLine();
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        checkoutDateString = input.nextLine();
+        checkinDate = LocalDate.parse(checkinDateString, dateTimeFormatter);
+        checkoutDate = LocalDate.parse(checkoutDateString, dateTimeFormatter);
+        reservation01.updateDates(checkinDate, checkoutDate);
+        for (Reservation r : reservationsList) {
+            System.out.println(r);
+        }
 
         input.close();
     }
