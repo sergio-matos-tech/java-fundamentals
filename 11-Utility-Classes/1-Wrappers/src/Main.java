3d1d90b30e528df7dcbc7a5663613541d9cd394b
shenrique08 <sergimmatos@hotmail.com>
2025-03-27 17:28:45 -0300

    Wrappers classes

---
+++ 11-Utility-Classes/1-Wrappers/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer integer = 10;
+        Float floater = 10f;
+        Long longer = 10L;
+        String string = "@345";
+        Character c = 'A';
+        Byte bytes = 100;
+    }
+}
