6208e950f45be0e2fc30796354ca073b3419b784
shenrique08 <sergimmatos@hotmail.com>
2025-03-27 18:01:13 -0300

    Working with String, StringBuffer and StringBuilder

--- 11-Utility-Classes/2-Strings/src/Main.java
+++ 11-Utility-Classes/2-Strings/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name1 = "      Sergio      ";
+        name1 = name1.trim();
+        String name2 = "Sergio";
+        System.out.println(name1);
+        System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
+        name1 = name1.concat(" Henrique");
+        // Strings are immutable
+        System.out.println(name1);
+        // name1 and name2 has the same reference in heap memory (String constant pool)
+        System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
     }
 }
---
+++ 11-Utility-Classes/3-StringBuilder/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("jfldsalkjdsaflkj");
+        stringBuilder.append("sgfakjNOME");
+        System.out.println(stringBuilder);  
+    }
+}
