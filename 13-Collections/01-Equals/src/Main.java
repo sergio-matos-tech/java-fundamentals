842aff4af98f3439c216ef537abf73ec71290ed1
shenrique08 <sergimmatos@hotmail.com>
2025-04-03 17:32:24 -0300

    Working with Equals

---
+++ 13-Collections/01-Equals/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Smartphone smartphone1 = new Smartphone("1APPLE15", "Apple", "Iphone 15");
+        Smartphone smartphone2 = new Smartphone("1APPLE15", "Apple", "Iphone 15 Plus");
+        Smartphone smartphone3 = new Smartphone("1APPLE16", "Apple", "Iphone 16");
+
+        System.out.println(smartphone1.equals(smartphone2));
+        System.out.println(smartphone1.equals(smartphone3));
+    }
+}
