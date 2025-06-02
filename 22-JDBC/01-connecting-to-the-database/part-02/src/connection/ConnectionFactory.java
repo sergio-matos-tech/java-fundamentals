a75b54378255867a6c8ece8a7e3d683a48b250d9
Sergio Henrique <sergimmatos@hotmail.com>
2025-06-02 16:38:42 -0300

    working with docker and connecting to the database using JDBC

---
+++ 22-JDBC/01-connecting-to-the-database/part-02/src/connection/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package connection;
+
+public class ConnectionFactory {
+    // java.sql = Connection, Statement, ResultSet, DriverMannager
+
+
+
+}
---
+++ 22-JDBC/01-connecting-to-the-database/part-03/src/main/java/org/application/App.java
@@ -0,0 +1,13 @@
+package org.application;
+
+
+import org.connection.ConnectionFactory;
+
+public class App {
+    public static void main(String[] args) {
+
+        ConnectionFactory.getConnection();
+
+
+    }
+}
